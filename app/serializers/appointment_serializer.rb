2b6e8d39134adf126ec1748bac074f588b8fb0f2
okikiola11 <okikiola.apelehin@gmail.com>
2021-09-22 18:45:35 +0000

    Add the gem for rack-cors

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :lawyer_name, :location, :appointed_date
+
+  belongs_to :lawyer
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module SymplyBookMeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configuration settings for Rack Cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put. :delete, :options]
+      end
+    end
   end
 end
