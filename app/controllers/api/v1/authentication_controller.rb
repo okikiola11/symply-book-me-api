982f71e270146d2f52b2f14f7923d10ded35fee3
okikiola11 <okikiola.apelehin@gmail.com>
2021-09-28 23:06:24 +0000

    Update the auth controller to accept secret key and generate token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -6,9 +6,12 @@ class Api::V1::AuthenticationController < ApplicationController
       render json: { error: 'Invalid username' }, status: :unauthorized
     else
       if user.authenticate(params[:password])
-        render json: { message: 'Correct password' }
+        secret_key = Rails.application.secrets.secret_key_base[0]
+        token = JWT.encode(user, secret_key)
+
+        render json: token
       else
-        render json: { error: 'Wrong password' },status: :unauthorized
+        render json: { error: 'Wrong password' }, status: :unauthorized
       end
     end
   end
