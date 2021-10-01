f7156d9dcec9889f1d689cd9145d715e1bd666d6
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-01 23:17:22 +0000

    Fix testing errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   has_many :lawyers
 
   validates :password, length: { in: 6..20 }, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { minimum: 2 }
   validates_confirmation_of :password
 end
