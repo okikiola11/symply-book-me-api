06d05698fdf7f23ecfa6490fcf9d62f63e3b42a7
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-01 15:21:37 +0000

    Add cloudinary image configuration setup

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -8,8 +8,8 @@ api_secret: ENV['CLOUD_API_SECRET'],
 )
 
 Shrine.storages = {
-cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
-store: Shrine::Storage::Cloudinary.new(prefix: "rails_uploads"),
+cache: Shrine::Storage::Cloudinary.new(prefix: "electric_car/cache"), # for direct uploads
+store: Shrine::Storage::Cloudinary.new(prefix: "electric_car"),
 }
 
 Shrine.plugin :activerecord           # loads Active Record integration
@@ -17,4 +17,3 @@ Shrine.plugin :cached_attachment_data # enables retaining cached file across for
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
 Shrine.plugin :validation_helpers
 Shrine.plugin :validation
-‍
