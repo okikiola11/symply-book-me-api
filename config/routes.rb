77c4c89134f27f58d9a1fca6031a87695c48d2eb
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-01 23:13:47 +0000

    Fix testing errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      root :to => "users#show"
+      # root :to => "users#show"
       resources :lawyers 
       resources :appointments, only: [:create, :index]
       # resources :sessions, only: [:create, :destroy]
