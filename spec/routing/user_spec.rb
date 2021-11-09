a2afbdc8a53a350c1e95633ccbadff87fff4bee1
okikiola11 <okikiola.apelehin@gmail.com>
2021-11-09 17:19:20 +0000

    Fix failing user test file

--- spec/routing/user_spec.rb
+++ spec/routing/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe 'registration routes' do
   it 'should route to users login session' do
-    expect(post('/api/v1/login')).to route_to('api/v1/authentication#login')
+    expect(post('/api/v1/create')).to route_to('api/v1/authentication#create')
   end
 end
